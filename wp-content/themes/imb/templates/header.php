c610328650e3ea244bcf1f9bc576a15d7b269d9e
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-01-13 10:10:45 +0000

    Initial commit

---
+++ wp-content/themes/imb/templates/header.php
@@ -0,0 +1,29 @@
+<header class="banner navbar navbar-default navbar-static-top" role="banner">
+  <div class="container">
+
+<div id="branding">
+<img alt="Law Commission Logo" src="wp-content/themes/imb/assets/img/imb-logo.jpg"/>
+</div>
+
+<div id="search">
+<?php get_search_form( $echo ); ?>
+</div>
+
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+  </div>
+
+    <nav class="collapse navbar-collapse" role="navigation">
+      <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+        endif;
+      ?>
+    </nav>
+  </div>
+</header>
---
+++ wp-content/themes/imb/templates/searchform.php
@@ -0,0 +1,9 @@
+<form role="search" method="get" class="search-form form-inline" action="<?php echo esc_url(home_url('/')); ?>">
+  <label class="sr-only"><?php _e('Search for:', 'roots'); ?></label>
+  <div class="input-group">
+    <input type="search" value="<?php echo get_search_query(); ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search the site', 'roots'); ?>">
+    <span class="input-group-btn">
+      <button type="submit" class="search-submit btn btn-default"><?php _e('', 'roots'); ?><img src="/lawcommission/wp-content/themes/wp-lawcom/assets/img/search-icon.gif"></button>
+    </span>
+  </div>
+</form>
