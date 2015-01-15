9d91686f86f241bb7c52190ae0c5d669ac719f3c
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-01-15 12:36:34 +0000

    Added CPT for reports

---
+++ wp-content/themes/imb/templates/content-report.php
@@ -0,0 +1,16 @@
+<?php while (have_posts()) : the_post(); ?>
+  <article <?php post_class(); ?>>
+    <header>
+      <h1 class="entry-title"><?php the_title(); ?></h1>
+
+      <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+
+    </header>
+    <div class="entry-content">
+        <?php the_content(); ?>
+    </div>
+    <footer>
+      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+    </footer>
+  </article>
+<?php endwhile; ?>
