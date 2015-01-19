e6141fc46c215e6853868b7ecbdfa2fccd261490
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-01-19 16:45:49 +0000

    Added columns to page template

--- wp-content/themes/imb/templates/content-page.php
+++ wp-content/themes/imb/templates/content-page.php
@@ -1,2 +1,17 @@
-<?php the_content(); ?>
-<?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+<div class="row">
+
+	<div class="col-sm-4">
+
+		<?php the_post_thumbnail('large');
+        	echo '<span class="img-caption">' . get_post(get_post_thumbnail_id())->post_content . '</span>'; 
+        ?>
+
+	</div>
+
+	<div class="col-sm-8">
+
+		<?php the_content(); ?>
+		<?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+
+	</div>
+</div>
