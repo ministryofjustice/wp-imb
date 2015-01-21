cb1932f4b8865cee20f4fab346708dcd69677bce
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-01-21 15:19:02 +0000

    Amended report cpt layout

--- wp-content/themes/imb/templates/content-report.php
+++ wp-content/themes/imb/templates/content-report.php
@@ -1,26 +1,57 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
-    <header>
+    <div class="page-header">
       <h1 class="entry-title"><?php the_title(); ?></h1>
-
-
-
-      <div class="news-meta">
-        <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
       </div>
+  </header>
 
-    </header>
-    <div class="entry-content">
+  <div class="row">
 
-        <?php the_excerpt(); ?>
+    <div class="col-sm-4">
 
-        <?php $fname = get_post_meta(get_the_ID(),'report-upload',true);  ?>
+        <div class="post-meta">
+          <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+        </div>
 
-        <a class="em-link float-right" href="<?php echo $fname; ?>">View/download report (PDF, 0.12Mb)</a>
+        <div class="download-report">
+        
+          <?php $fname = get_post_meta(get_the_ID(),'report-upload',true);  ?>
 
-        <!--Attachment ID and filesize need to be made dynamic -->
+          <a class="" href="<?php echo $fname; ?>">View/download report (PDF, 
+                <?php 
+
+                   //global $post;
+
+                   $post_id = get_the_ID();
+
+                  $myfile = filesize( get_attached_file( 68 ) ); 
+
+                  $docsize = size_format($myfile);
+
+                  echo $docsize;
+
+                  //echo $post_id;
+                  
+                  ?>
+              )
+          </a>
+            <!--Filesize need to be made dynamic -->
+       </div>
 
     </div>
+
+    <div class="col-sm-8">
+
+      <div class="entry-content">
+
+          <?php the_content(); ?>
+
+      </div>
+
+    </div>
+
+  </div>
+
     <footer>
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
     </footer>
