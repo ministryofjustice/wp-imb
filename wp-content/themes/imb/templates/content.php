974afb54fdcef2c30593f33d393c4038eea48e1f
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-03-05 15:29:52 +0000

    Report links in search results now link directly to PDFs

--- wp-content/themes/imb/templates/content.php
+++ wp-content/themes/imb/templates/content.php
@@ -1,6 +1,13 @@
 <article <?php post_class(); ?>>
   <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <h2 class="entry-title">
+      <?php $file = get_post_meta($post->ID, "report-upload"); ?>
+      <?php if($post->post_type == 'report' && !empty($file[0])): ?>
+	    <a href="<?= $file[0]; ?>"><?php the_title(); ?></a>
+	  <?php else: ?>
+	    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	  <?php endif; ?>	
+    </h2>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
   <div class="entry-summary">
