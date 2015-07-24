052acc2cf1707e99c4c2505c5b233bafe4af47ce
Toby Schrapel <toby@tobyschrapel.com>
2015-07-24 10:12:44 +0100

    Add tracking code

--- wp-content/themes/imb/templates/footer.php
+++ wp-content/themes/imb/templates/footer.php
@@ -6,4 +6,25 @@
   </div>
 </footer>
 
+<?php if(is_page('current-vacancies')): ?>
+<!-- Google Code for IMB Apply Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 973375943;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "OyiqCJf02F4Qx5OS0AM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/973375943/?label=OyiqCJf02F4Qx5OS0AM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+<?php endif; ?>
+
 <?php wp_footer(); ?>
