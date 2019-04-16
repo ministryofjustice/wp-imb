898d02b8cd3d0dea2698282e22f61e607892bed3
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2019-04-16 20:47:32 +0100

    Improve build + update plugins

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM mojdigital/wordpress-base:upgrade
 
-RUN curl -sL https://deb.nodesource.com/setup_8.x | bash - && \
+RUN curl -sL https://deb.nodesource.com/setup_11.x | bash - && \
     DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/* /var/tmp/* /init
@@ -16,7 +16,7 @@ RUN echo "{ \"allow_root\": true }" > /root/.bowerrc
 
 # Set execute bit permissions before running build scripts
 RUN chmod +x bin/* && sleep 1 && \
-    make clean && \
+    #make clean && \
     bin/composer-auth.sh && \
     make build && \
     rm -f auth.json
--- web/app/themes/imb/page-faqs.php
+++ web/app/themes/imb/page-faqs.php
@@ -1,4 +1,3 @@
-
 <?php
 /*
 Template Name: FAQs page
@@ -6,74 +5,77 @@ Template Name: FAQs page
 ?>
 
 
-<?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('templates/page', 'header'); ?>
+<?php while (have_posts()) :
+the_post(); ?>
+<?php get_template_part('templates/page', 'header'); ?>
 
-  	<div class="row">
-   		<div class="col-sm-4 min-col">
+<div class="row">
+	<div class="col-sm-4 min-col">
 
-			<?php
-				$get_description = get_post(get_post_thumbnail_id())->post_excerpt;
-				echo '<div class="page-img-wrapper">';
-				the_post_thumbnail('large');
-				echo '</div>';
-				  if(!empty($get_description)){//If description is not empty show the div
-				  echo '<div class="img-caption">' . get_post(get_post_thumbnail_id())->post_excerpt . '</div>';
-				  }
-			?>
+		<?php
+		$get_description = get_post(get_post_thumbnail_id())->post_excerpt;
+		echo '<div class="page-img-wrapper">';
+		the_post_thumbnail('large');
+		echo '</div>';
+		if (!empty($get_description)) {//If description is not empty show the div
+			echo '<div class="img-caption">' . get_post(get_post_thumbnail_id())->post_excerpt . '</div>';
+		}
+		?>
 
-<?php endwhile; ?>
+		<?php endwhile; ?>
 
-		  </div>
+	</div>
 
-  		<div class="col-sm-8 max-col">
+	<div class="col-sm-8 max-col">
 
-  			<?php the_content(); ?>
+		<?php the_content(); ?>
 
 
-	  		<?php
-			    $args = array(
-			        'post_type' => 'faq',
-			        'orderby' => 'menu_order',
-			        'order' => 'ASC',
-			        'posts_per_page' => 20
-			    );
-			    $the_query = new WP_Query( $args );
-			?>
+		<?php
+		$args = array(
+			'post_type' => 'faq',
+			'orderby' => 'menu_order',
+			'order' => 'ASC',
+			'posts_per_page' => 20
+		);
+		$the_query = new WP_Query($args);
+		?>
 
-	<div class="card-group" id="accordion" role="tablist" aria-multiselectable="true">
+		<div class="card-group" id="accordion" role="tablist" aria-multiselectable="true">
 
-		<?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+			<?php if (have_posts()) : while ($the_query->have_posts()) : $the_query->the_post(); ?>
 
-		  <div class="card card-default">
-		    <div class="card-heading" role="tab" id="headingOne">
-		      <h4 class="card-title">
-		        <a data-toggle="collapse" href="#collapse<?php the_ID(); ?>" aria-expanded="true" aria-controls="collapse<?php the_ID(); ?>">
-		          <?php the_title(); ?>
-		        </a>
-		      </h4>
-		    </div>
-		    <div id="collapse<?php the_ID(); ?>" class="card-collapse collapse" role="tabpanel" data-parent="#accordion" aria-labelledby="heading<?php the_ID(); ?>">
-		      <div class="card-body">
-		        <?php the_content(); ?>
-		      </div>
-		    </div>
-		  </div>
+				<div class="card card-default">
+					<div class="card-heading" role="tab" id="headingOne">
+						<h4 class="card-title">
+							<a data-toggle="collapse" href="#collapse<?php the_ID(); ?>" aria-expanded="true"
+							   aria-controls="collapse<?php the_ID(); ?>">
+								<?php the_title(); ?>
+							</a>
+						</h4>
+					</div>
+					<div id="collapse<?php the_ID(); ?>" class="card-collapse collapse" role="tabpanel"
+						 data-parent="#accordion" aria-labelledby="heading<?php the_ID(); ?>">
+						<div class="card-body">
+							<?php the_content(); ?>
+						</div>
+					</div>
+				</div>
 
-		<?php endwhile; else: ?>
+			<?php endwhile; else: ?>
 
-		    <p>No questions here yet.</p>
+				<p>No questions here yet.</p>
 
-		<?php endif; ?>
-		<?php wp_reset_postdata(); ?>
+			<?php endif; ?>
+			<?php wp_reset_postdata(); ?>
 
 		</div><!--end of the accordion wrap-->
 
-		  </div>
-		</div>
+	</div>
+</div>
 
 
-  		</div>
+</div>
 
 </div>
 
