30915b7a708cb12f3e6e934f9d0032675330d51f
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2019-05-08 11:20:18 +0100

    Update scripts

--- web/app/themes/imb/lib/scripts.php
+++ web/app/themes/imb/lib/scripts.php
@@ -68,7 +68,8 @@ function roots_jquery_local_fallback($src, $handle = null)
 	static $add_jquery_fallback = false;
 
 	if ($add_jquery_fallback) {
-		echo '<script>window.jQuery || document.write(\'<script src="$local"></script>\');</script>' . "\n";
+		$local = get_template_directory_uri() . '/assets/js/jquery.js';
+		echo "<script>window.jQuery || document.write('<script src=\"$local\"><\/script>');</script>\n";
 		$add_jquery_fallback = false;
 	}
 
