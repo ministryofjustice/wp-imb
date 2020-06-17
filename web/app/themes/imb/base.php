ed8779c58b27243804eaff4b4f598192fd9ce443
Beverley Newing <bnewing92@hotmail.co.uk>
2020-06-17 18:06:04 +0100

    Remove social media icons added in by addthis
    Addthis add in additional cookies, which are non-essential. We're trialling a new cookie banner on the IMB site, and this would complicate that banner. So we're removing this for now, whilst we run that test.

--- web/app/themes/imb/base.php
+++ web/app/themes/imb/base.php
@@ -24,8 +24,5 @@
 
 	<?php get_footer(); ?>
 
-  <!-- Go to www.addthis.com/dashboard to customize your tools -->
-  <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-55d49ccb5d8a5bba" async="async"></script>
-
 </body>
 </html>
