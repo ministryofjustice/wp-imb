9414654a01c463d9b1f0c3ced2fa9ad3a6de38e3
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2020-07-01 15:41:40 +0100

    report links in sitemap

--- web/app/themes/imb/lib/cpt/report.php
+++ web/app/themes/imb/lib/cpt/report.php
@@ -20,3 +20,20 @@ function report_taxonomy() {
 }
 
 add_action( 'init', 'report_taxonomy', 0 );
+
+function report_update_sitemap_link($url, $post)
+{
+    if ('report' == get_post_type($post)) {
+
+        $report_url = get_post_meta($post->ID, 'report-upload',true);
+
+
+        if (!empty($report_url)) {
+            $url = $report_url;
+        }
+
+    }
+
+    return $url;
+}
+add_filter( 'wsp_cpt_link', 'report_update_sitemap_link', 10, 2 );
