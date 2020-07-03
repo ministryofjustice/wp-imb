fd0efaa0e2c4efafb159d718e07787b9d87bb73b
Beverley Newing <bnewing92@hotmail.co.uk>
2020-07-03 16:52:05 +0100

    Add cookie banner variant B
    We are testing two new cookie banners, and variant B will be hosted on IMB. This PR allows it to pick this new version from Satis, and also makes some changes to the theme to make sure it loads in correctly.

--- web/app/themes/imb/base.php
+++ web/app/themes/imb/base.php
@@ -1,5 +1,4 @@
 <?php get_header(); ?>
-<body <?php body_class(); ?>>
 
   <!--[if lt IE 8]>
 	<div class="alert alert-warning">
@@ -7,22 +6,19 @@
 	</div>
   <![endif]-->
 
-	<?php
-	do_action( 'get_header' );
-	get_header();
-	?>
-
   <div class="wrap container" role="document">
-	<div class="content row">
-	  <main class="main" role="main">
-		<?php require roots_template_path(); ?>
-	  </main><!-- /.main -->
-		<?php if ( roots_display_sidebar() ) : ?>
-		<?php endif; ?>
-	</div><!-- /.content -->
+		<div class="content row">
+			<main class="main" role="main">
+			<?php require roots_template_path(); ?>
+			</main><!-- /.main -->
+			<?php if ( roots_display_sidebar() ) : ?>
+			<?php endif; ?>
+		</div><!-- /.content -->
   </div><!-- /.wrap -->
 
 	<?php get_footer(); ?>
+
+	</div><!-- /.ccfw-banner-background class set in header -->
 
 </body>
 </html>
